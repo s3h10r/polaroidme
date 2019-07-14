9b6aefc835484548e8b76c94a1161b8455997638
s3h10r <sven.hessenmueller@gmail.com>
2019-07-15 00:18:39 +0200

    fixes build & test-install script

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ rm -Rf __pycache__
 
 # apt-get install python3-setuptools
 #python3 setup.py bdist_wheel
-python3 setup.py sdist --formats=gztar,bztar,zip
+#python3 setup.py sdist --formats=gztar,bztar,zip
+python3 setup.py sdist --formats=gztar
--- test_build.sh
+++ test_build.sh
@@ -6,10 +6,10 @@ mkdir -p ./tmp/tests/
 python3 -m venv ./tmp/venv_polaroidme
 source ./tmp/venv_polaroidme/bin/activate
 #pip3 install ./dist/polaroidme-0.8.6-py3-none-any.whl
-pip3 install ./dist/polaroidme-0.8.6.tar.bz2
+pip3 install ./dist/polaroidme-0.8.6.tar.gz
 find ./tmp/venv_polaroidme/ -name "*ttf"
 cp ./examples/example.ps-10.png ./tmp/tests/
-polaroidme ./tmp/tests/example.ps-10.png 800 center "installed via pip"
+polaroidme ./tmp/tests/example.ps-10.png 800 center "test_build.sh - a fresh build installed via pip"
 feh ./tmp/tests/example.ps-10.polaroid.png
 rm -Rf ./tmp/
 #deactivate
