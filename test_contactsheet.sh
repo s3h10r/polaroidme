bb3d1481ad3df567534e3de5078a6e0f7805e402
s3h10r <sven.hessenmueller@gmail.com>
2019-07-15 23:54:28 +0200

    implements script to create contact sheets

---
+++ test_contactsheet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./contactsheet.py ./examples/ && feh contactsheet.png
+./contactsheet.py ./examples/ free && feh contactsheet.png
+./contactsheet.py ./examples/ square 400 && feh contactsheet.png
