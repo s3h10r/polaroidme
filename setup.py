02fe952c6fecbe0b006cb940826862f5242731e2
s3h10r <sven.hessenmueller@gmail.com>
2019-07-23 15:19:22 +0200

    tidies up a bit

--- setup.py
+++ setup.py
@@ -12,28 +12,50 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 long_description_md = """
-polaroidme is a simple to use command-line-tool for placing an image into a
-Polaroid-like frame and optionally put a title / description on the bottom.
-The default font mimics scribbled handwriting but any (ttf-)font
-which suits your taste is supported. The tool offers basic features
-like auto-scaling up-/downwards and/or cropping, using any (ttf-)font,
-supports high-res output and gets the job just done well.
-polaroidme is actively maintained & developed (2019).
-To see if it fits your needs take a look at the project's
+polaroidme - vintage polaroid style + generative art
+====================================================
+
+polaroidme is a simple command-line-tool & python-library for placing an image
+into a Polaroid-like frame and optionally put a title / description or meta infos
+out of EXIF-data on the bottom. The default font mimics scribbled handwriting
+but any (ttf-)font which suits your taste is supported. The tool offers basic
+features like auto-scaling up-/downwards and/or cropping, using any (ttf-)font,
+supports high-res output and gets it's job done well.
+
+Starting as `a script for making high-res contactsheets which make the beholder's eyes not bleed <https://github.com/s3h10r/fokus>`_ recently polaroidme gets phonky by providing:
+
+* generative-art-"generators" which are fun to play with
+* chainable filters (`--filter f1,f2,...,fN`)
+* reading EXIF-data
+* lightweight plugin-framework - makes it simple to use (and write) additional filters & generators
+* support for templates
+* `--help`-option :)
+
+The author's main usecase for polaroidme is doing minimalistic artworks in
+printing quality with it - and learning and mixing fun to use technologies
+full-stack around it (raspi, webapis, flutter). :D
+
+To see if it could be usefull for your needs take a look at the project's
 github-repo and check out the `examples`_
 
+polaroidme is actively maintained & developed (2019).
+
 Contributions are welcome, and they are greatly appreciated!
+
+Please feel free to send pull-requests an use the `issue tracker <https://github.com/s3h10r/polaroidme/issues>`_.
+
+Have fun!
 
 _`examples`: https://github.com/s3h10r/polaroidme/blob/master/README.md
 """
 
 setuptools.setup(
      name='polaroidme',
-     version='0.9.38',
+     version='0.9.39',
      scripts=['cli/polaroidme', 'cli/pom','cli/contactsheet'] ,
      author="Sven Hessenmüller",
      author_email="sven.hessenmueller@gmail.com",
-     description="converts an image into vintage polaroid style",
+     description="converts an image into vintage polaroid style - and can do some phonky stuff. :D",
      include_package_data=True,
      long_description=long_description_md,
      url="https://github.com/s3h10r/polaroidme",
