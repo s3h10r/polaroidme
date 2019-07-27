92d829d54d1f1996b5e740bbc7e707ab67acb201
s3h10r <sven.hessenmueller@gmail.com>
2019-07-27 16:41:10 +0200

    goodbye polaroidme, hello einguteswerkzeug :)

--- update-plugins.py
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -vx
-git submodule add https://github.com/s3h10r/polaroidme-plugins polaroidme/plugins
-git submodule add https://github.com/s3h10r/random-art polaroidme/plugins/generators/random-art
-git submodule update
